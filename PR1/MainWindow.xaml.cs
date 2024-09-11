ca6c98120a65a559a1284a06ea0e35e78ff49b69
Клим Антонов < @42-5>
2024-09-11 10:01:18 +0500

    Добавьте файлы проекта.

---
+++ PR1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+                
+                Grid matrixGrid = new Grid();
+                matrixGrid.ShowGridLines = true;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Button button = new Button();
+                        button.Content = $"[{i},{j}]";
+                        button.Margin = new Thickness(5);
+
+                        Grid.SetRow(button, i);
+                        Grid.SetColumn(button, j);
+                        matrixGrid.Children.Add(button);
+                    }
+                }
+                matrixGrid.RowDefinitions.Add(new RowDefinition());
+                matrixGrid.RowDefinitions.Add(new RowDefinition());
+                matrixGrid.RowDefinitions.Add(new RowDefinition());
+
+                matrixGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                matrixGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                matrixGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                
+                MainGrid.Children.Add(matrixGrid);
+            }
+        }
+    }
+
+   
